b09f4204af3be66864d72e400224127b4d9ce301
Maykel Arias Torres <dev0006@simelo.tech>
2020-04-04 23:42:53 -0400

    Creado el create de Create Form

--- products/forms.py
+++ products/forms.py
@@ -3,6 +3,21 @@ from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    title = forms.CharField(label="Insert here", widget=forms.TextInput(attrs={
+        "placeholder": "Your title",
+    }))
+    description = forms.CharField(required=False, widget=forms.Textarea(
+        attrs={
+            "class": "new-class-name two",
+            "placeholder": "Your description",
+            "id": "my-id-for-textarea",
+            "rows": 20,
+            "cols": 120,
+        }
+    ))
+    price = forms.DecimalField(initial=199.99)
+    email = forms.EmailField()
+
     class Meta:
         model = Product
         fields = [
@@ -10,3 +25,28 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get("title")
+        if "CFE" in title:
+            return title
+        else:
+            raise forms.ValidationError("This is not a valid title")
+
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get("email")
+        if not email.endswith("edu"):
+            raise forms.ValidationError("This is not a valid email")
+
+
+class RawProductForm(forms.Form):
+    title = forms.CharField(label="")
+    description = forms.CharField(required=False, widget=forms.Textarea(
+        attrs={
+            "class": "new-class-name two",
+            "id": "my-id-for-textarea",
+            "rows": 20,
+            "cols": 20,
+        }
+    ))
+    price = forms.DecimalField(initial=199.99)
--- products/views.py
+++ products/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 
 
 # Create your views here.
+
+# def product_create_view(request):
+#     form = RawProductForm()
+#     if request.method == "POST":
+#         form = RawProductForm(request.POST)
+#         if form.is_valid():
+#             # now the data is good
+#             print(form.cleaned_data)
+#             Product.objects.create(**form.cleaned_data)
+#         else:
+#             print(form.errors)
+#
+#     context = {
+#         "form": form
+#     }
+#     return render(request, "products/product_create.html", context)
+#
+
+# def product_create_view(request):
+#     # print(request.GET)
+#     # print(request.POST)
+#     if request.method == "POST":
+#         title = request.POST.get("title")
+#         print(title)
+#         # Product.object.create
+#     context = {}
+#     return render(request, "products/product_create.html", context)
+
 
 def product_create_view(request):
     form = ProductForm(request.POST or None)
